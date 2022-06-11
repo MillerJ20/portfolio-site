ebcfe42b0c8b2db9965d9a64ecabaa15e5215a3d
MillerJ20 <miller.johnston17@gmail.com>
2022-06-11 00:23:54 -0700

    Created Navbar, imported Prettier. Need to find proper type to return NavLinks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Navbar } from './components/Navbar';
 import './App.css';
 
+const navigation = {
+  brand: { name: 'NavbarScroller', to: '/' },
+  links: [
+    { name: 'About Me', to: '/about' },
+    { name: 'Technologies', to: '/technologies' },
+    { name: 'Recent Work', to: '/recentWork' }
+  ]
+};
+
 function App() {
+  const { brand, links } = navigation;
   return (
     <div className="App">
-      Test Push
+      <Navbar brand={brand} links={links} />
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Navbar = (props: {
+  brand: { name: string; to: string };
+  links: Array<{ name: string; to: string }>;
+}) => {
+  const { brand, links } = props;
+  //const NavLinks: HTMLElement = () => links.map((link: { name: string, to: string }) => <li key={link.name}><a href={link.to}>{link.name}</a></li>);
+  return (
+    <div>
+      <a href={brand.to}> {brand.name}</a>
+      {/* <NavLinks /> */}
+    </div>
+  );
+};
