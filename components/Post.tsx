36e73e9ab40628a26cf25da8ab44a6b16c47d972
Miller Johnston <millerjohnston@pop-os.localdomain>
2023-05-30 00:15:53 -0700

    Card styling adjustments for consistency sake

--- components/Post.tsx
+++ components/Post.tsx
@@ -4,10 +4,10 @@ const Post = ({post}) => {
   return (
     <div className="card">
       <img src={post.frontmatter.cover_image} alt='Blog-Post Cover Image'/>
-      <div className="post-date"> Posted on {post.frontmatter.date}</div>
-      <h3>{post.frontmatter.title}</h3>
-      <p>{post.frontmatter.excerpt}</p>
-      <Link href={`/blog/${post.slug}`} className='btn'>Read More</Link>
+      <div className="card-date"> Posted on {post.frontmatter.date}</div>
+      <h3 className="card-title">{post.frontmatter.title}</h3>
+      <p className="card-excerpt">{post.frontmatter.excerpt}</p>
+      <Link href={`/blog/${post.slug}`} className='btn' id='post-btn'>Read More</Link>
     </div>
   )
 }
