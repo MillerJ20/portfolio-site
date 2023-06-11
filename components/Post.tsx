c5a6c84c20043262057b0500b407048d44cfa439
Miller Johnston <miller.johnston17@gmail.com>
2023-06-11 15:49:37 -0700

    Added useWindowDimensions hook for responsiveness, and fixed Chrome/Firefox inconsistencies... for now

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,26 +1,11 @@
 import Link from "next/link";
-import { useEffect, useState } from "react";
-
+import useWindowDimensions from "../hooks/useWindowDimensions"
 const Post = ({ post }) => {
-  const [windowWidth, setWindowWidth] = useState(0);
-  const [windowHeight, setWindowHeight] = useState(0);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-      setWindowHeight(window.innerHeight);
-    }
-    window.addEventListener('resize', handleResize);
-    console.log("Height: " + windowHeight);
-    console.log("Width: " + windowWidth);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, [windowHeight, windowWidth]);
+  const { height, width } = useWindowDimensions();
 
   return (
     <>
-      {windowWidth >= 900 && windowHeight > 800 ? (
+      {width >= 900 && height > 800 ? (
         <div className="card">
           <img src={post.frontmatter.cover_image} alt='Blog-Post Cover Image' />
           <div className="card-date"> Posted on {post.frontmatter.date}</div>
