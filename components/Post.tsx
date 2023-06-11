9f6492f387217a2afcc9a25f6d4bb1f7169ab11d
Miller Johnston <miller.johnston17@gmail.com>
2023-06-11 00:37:53 -0700

    Attempting to normalize

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,14 +1,42 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
-const Post = ({post}) => {
+const Post = ({ post }) => {
+  const [windowWidth, setWindowWidth] = useState(0);
+  const [windowHeight, setWindowHeight] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+      setWindowHeight(window.innerHeight);
+    }
+    window.addEventListener('resize', handleResize);
+    console.log("Height: " + windowHeight);
+    console.log("Width: " + windowWidth);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [windowHeight, windowWidth]);
+
   return (
-    <div className="card">
-      <img src={post.frontmatter.cover_image} alt='Blog-Post Cover Image'/>
-      <div className="card-date"> Posted on {post.frontmatter.date}</div>
-      <h3 className="card-title">{post.frontmatter.title}</h3>
-      <p className="card-excerpt">{post.frontmatter.excerpt}</p>
-      <Link href={`/blog/${post.slug}`} className='btn' id='post-btn'>Read More</Link>
-    </div>
+    <>
+      {windowWidth >= 900 && windowHeight > 800 ? (
+        <div className="card">
+          <img src={post.frontmatter.cover_image} alt='Blog-Post Cover Image' />
+          <div className="card-date"> Posted on {post.frontmatter.date}</div>
+          <h3 className="card-title">{post.frontmatter.title}</h3>
+          <p className="card-excerpt">{post.frontmatter.excerpt}</p>
+          <Link href={`/blog/${post.slug}`} className='btn' id='post-btn'>Read More</Link>
+        </div>
+      ) : (
+        <div className="card-small-screen">
+          <div className="card-date-small"> Posted on {post.frontmatter.date}</div>
+          <h3 className="card-title-small">{post.frontmatter.title}</h3>
+          <p className="card-excerpt-small">{post.frontmatter.excerpt}</p>
+          <Link href={`/blog/${post.slug}`} className='btn' id='post-btn'>Read More</Link>
+        </div>
+      )}
+    </>
   )
 }
 
